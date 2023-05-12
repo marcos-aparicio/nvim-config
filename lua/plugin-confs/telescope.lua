4b1251733a061631149990ef8ee20c05342d5f41
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-05-12 17:34:46 -0400

    modified bufferline styles so it looks better on my system

--- lua/plugin-confs/telescope.lua
+++ lua/plugin-confs/telescope.lua
@@ -1,20 +1,20 @@
 -- this file also includes all treesitter extensions
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 local actions = require("telescope.actions")
 
 telescope.setup({
-  defaults = { path_display = { "shorten" } },
-  mappings = {
-    i = {
-      ["<C-c>"] = actions.close,
-      ["<C-x>"] = actions.select_horizontal,
-      ["<C-v>"] = actions.select_vertical,
-    },
-    n = {
-      ["q"] = actions.close,
-    },
-  },
+	defaults = { path_display = { "shorten" } },
+	mappings = {
+		i = {
+			["<C-c>"] = actions.close,
+			["<C-x>"] = actions.select_horizontal,
+			["<C-v>"] = actions.select_vertical,
+		},
+		n = {
+			["q"] = actions.close,
+		},
+	},
 })
