285100b7bd9880588267e5e21e4f30b0135ea297
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-05-27 12:25:00 -0400

    adding new configuration in workspaces so each project can have a default file that will be opened

--- lua/plugin-confs/workspaces.lua
+++ lua/plugin-confs/workspaces.lua
@@ -2,8 +2,24 @@ local ok, workspaces = pcall(require, "workspaces")
 if not ok then
 	return
 end
+
+function fileToOpenPerProject()
+	local current_workspace = workspaces.name()
+	local ok, files_to_open = pcall(require, "plugin-confs.workspaces-private-config")
+
+	if not ok then
+		return
+	end
+
+	local file_to_open = files_to_open[current_workspace]
+	if file_to_open == nil then
+		return
+	end
+	vim.api.nvim_command("e " .. file_to_open)
+end
+
 workspaces.setup({
 	hooks = {
-		open = { "NvimTreeRefresh", "Telescope find_files" },
+		open = { fileToOpenPerProject, "NvimTreeRefresh", "NvimTreeOpen" },
 	},
 })
