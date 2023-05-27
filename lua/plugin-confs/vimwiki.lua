ea24361530bfb2917c736c1e04cc1c708cb2d91e
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-05-27 13:41:11 -0400

    creating a custom command to update links on index file of vimwiki

--- lua/plugin-confs/vimwiki.lua
+++ lua/plugin-confs/vimwiki.lua
@@ -1,1 +1,2 @@
-vim.api.nvim_set_var("vimwiki_list", { { path = "~/documents/vimwiki", syntax = "markdown", ext = ".md" } })
+vim.api.nvim_set_var("vimwiki_list", { { path = "~/Documents/vimwiki", syntax = "markdown", ext = ".md" } })
+vim.cmd("command! Vwl lua vim.cmd('VimwikiGenerateLinks')")
