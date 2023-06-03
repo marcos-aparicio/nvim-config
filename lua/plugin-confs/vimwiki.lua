a0314342d6e9c0c346a29509aca9a91dbb53300a
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-06-03 15:43:19 -0400

    removing command from vimwiki.lua since i don't longer need it

--- lua/plugin-confs/vimwiki.lua
+++ lua/plugin-confs/vimwiki.lua
@@ -1,10 +1,9 @@
 vim.api.nvim_set_var(
-  "vimwiki_list",
-  { {
-    path = "~/Documents/vimwiki",
-    syntax = "markdown",
-    ext = ".md",
-    auto_generate_links = 1,
-  } }
+	"vimwiki_list",
+	{ {
+		path = "~/Documents/vimwiki",
+		syntax = "markdown",
+		ext = ".md",
+		auto_generate_links = 1,
+	} }
 )
-vim.cmd("command! Vwl lua vim.cmd('VimwikiGenerateLinks')")
