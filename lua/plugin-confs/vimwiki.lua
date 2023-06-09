c284197499a8ecef1936226806c3c0851b4c212f
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-06-09 19:25:01 -0400

    fixing vimwiki only keybindings

--- lua/plugin-confs/vimwiki.lua
+++ lua/plugin-confs/vimwiki.lua
@@ -1,5 +1,5 @@
-local vimwiki_path = "~/Obsidian/obsidian/C"
-local vimwiki_path2 = "~/Obsidian/obsidian/D"
+local vimwiki_path = os.getenv("HOME") .. "/Obsidian/obsidian/C"
+local vimwiki_path2 = os.getenv("HOME") .. "/Obsidian/obsidian/D"
 vim.api.nvim_set_var("vimwiki_list", {
 	{
 		path = vimwiki_path,
@@ -17,13 +17,42 @@ vim.api.nvim_set_var("vimwiki_list", {
 vim.g.wiki_auto_header = 1
 
 vim.cmd([[
-augroup vimwiki_mappings
-  autocmd!
-  autocmd FileType vimwiki nnoremap <Leader>f :lua require'telescope.builtin'.find_files({ cwd = ']] .. vimwiki_path .. [[', prompt_title = 'vimwiki',previewer=false })<CR>
-  autocmd FileType vimwiki nnoremap <Leader>l :Telescope vimwiki live_grep<CR>
-  autocmd FileType vimwiki nnoremap <Leader>x :w ~/.local/share/Trash/files/%:t \| :VimwikiDeleteFile <CR>y<CR>
-augroup END
+  augroup vimwiki_mappings
+    autocmd!
+    autocmd FileType vimwiki lua set_vimwiki_mappings()
+  augroup END
 ]])
---[[ autocmd FileType vimwiki nnoremap <Leader>wd :w ~/.local/share/Trash/files/%:t<CR> ]]
---[[ autocmd FileType vimwiki nnoremap <Leader>x :w ~/.local/share/Trash/files/%:t<CR>:VimwikiDeleteFile<CR> ]]
---
+
+function set_vimwiki_mappings()
+	local buffer = vim.api.nvim_get_current_buf()
+	local buffer_path = vim.api.nvim_buf_get_name(buffer)
+	print(buffer_path)
+
+	if buffer_path:match(vimwiki_path) then
+		vim.api.nvim_buf_set_keymap(
+			buffer,
+			"n",
+			"<Leader>f",
+			':lua require"telescope.builtin".find_files({ cwd = "'
+				.. vimwiki_path
+				.. '", prompt_title = "vimwiki", previewer = false })<CR>',
+			{ noremap = true }
+		)
+		vim.api.nvim_buf_set_keymap(buffer, "n", "<Leader>l", ":Telescope vimwiki live_grep<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(
+			buffer,
+			"n",
+			"<Leader>x",
+			":w ~/.local/share/Trash/files/%:t<Bar>:VimwikiDeleteFile<CR>y<CR>",
+			{ noremap = true }
+		)
+	end
+end
+
+--[[ function clear_vimwiki_mappings() ]]
+--[[ 	local buffer = vim.api.nvim_get_current_buf() ]]
+--[[]]
+--[[ 	vim.api.nvim_buf_del_keymap(buffer, "n", "<Leader>f") ]]
+--[[ 	vim.api.nvim_buf_del_keymap(buffer, "n", "<Leader>l") ]]
+--[[ 	vim.api.nvim_buf_del_keymap(buffer, "n", "<Leader>x") ]]
+--[[ end ]]
