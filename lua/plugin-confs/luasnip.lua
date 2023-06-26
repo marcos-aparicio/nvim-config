32cb4471d8871dc9f2895c30c318c288b5677dd5
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-06-26 17:21:22 -0400

    adding a snippet to markdown files

---
+++ lua/plugin-confs/luasnip.lua
@@ -0,0 +1,10 @@
+local ok, ls = pcall(require, "luasnip")
+if not ok then
+	return
+end
+local s = ls.snippet
+local t = ls.text_node
+
+ls.add_snippets("vimwiki", {
+	s("todo", { t("- [ ] ") }),
+})
