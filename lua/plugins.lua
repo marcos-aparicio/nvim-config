15a2f77bbb33f530cd300fd653a43f7a5704114c
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-06-30 07:45:07 -0400

    adding octo-nvim plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -144,6 +144,15 @@ packer.startup(function(use)
 		end,
 	})
 
+	use({
+		"pwntester/octo.nvim",
+		--[[ requires = { ]]
+		--[[ 	"nvim-lua/plenary.nvim", ]]
+		--[[ 	"nvim-telescope/telescope.nvim", ]]
+		--[[ 	"nvim-tree/nvim-web-devicons", ]]
+		--[[ }, ]]
+	})
+
 	use("tpope/vim-dispatch")
 
 	-- Automatically set up your configuration after cloning packer.nvim
