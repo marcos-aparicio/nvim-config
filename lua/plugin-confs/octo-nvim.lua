68fb2fd0e7e3b864cb7f866926e0827d143e3a61
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-07-07 23:26:25 -0400

    adding new mappings to octo nvim to review and reload a PR

--- lua/plugin-confs/octo-nvim.lua
+++ lua/plugin-confs/octo-nvim.lua
@@ -6,3 +6,5 @@ end
 octo.setup()
 M.nmap("<leader>opl", ":Octo pr list<CR>")
 M.nmap("<leader>ope", ":Octo pr edit<space>")
+M.nmap("<leader>opr", ":Octo pr reload<CR>")
+M.nmap("<leader>or", ":Octo review<space>")
