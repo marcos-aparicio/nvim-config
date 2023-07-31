c2dbb7bb433b96a5444c46317d209285a02a8c10
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-07-31 12:17:50 -0400

    changing the tabs priority in completions

--- lua/plugin-confs/completions.lua
+++ lua/plugin-confs/completions.lua
@@ -34,10 +34,10 @@ cmp.setup({
 
 		-- the TABS
 		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+			if luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif cmp.visible() then
+				cmp.select_next_item()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -45,10 +45,10 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
+			elseif cmp.visible() then
+				cmp.select_prev_item()
 			else
 				fallback()
 			end
