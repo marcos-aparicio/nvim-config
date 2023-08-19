3ecd346196e52e803fcb5cbb92953156c4e325f1
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-08-19 18:25:54 -0400

    creating a command in nvim that will open the current buffer in another alacritty terminal window

--- lua/commands.lua
+++ lua/commands.lua
@@ -7,3 +7,21 @@ end
 
 -- Command to call the Lua function
 vim.cmd([[command! CopyBufferPath :lua copy_buffer_path()]])
+
+-- Add a custom command to open Alacritty with the current buffer in read-only mode
+vim.cmd([[
+  command! -nargs=0 OpenAlacrittyReadonly :lua OpenAlacrittyReadonly()
+]])
+
+-- Function to open Alacritty with the current buffer in read-only mode
+function OpenAlacrittyReadonly()
+	local current_file = vim.fn.expand("%:p")
+	local current_line = vim.fn.line(".")
+	local current_col = vim.fn.col(".")
+
+	local alacritty_command =
+		string.format('alacritty --command nvim -c "set readonly" +%d,%d %s', current_line, current_col, current_file)
+	vim.fn.jobstart(alacritty_command, {
+		detach = true,
+	})
+end
