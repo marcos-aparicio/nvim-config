2029700ac91b69b9e24082bd4dd89680a255910e
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-09-09 22:29:01 -0400

    deleting nvim rainbow config as it is not needed

--- lua/plugin-confs/treesitter.lua
+++ lua/plugin-confs/treesitter.lua
@@ -4,22 +4,6 @@ if not status_ok then
 	return
 end
 
-local rainbow_ok, rb = pcall(require, "ts-rainbow")
-local rainbow_config = {}
-if rainbow_ok then
-	rainbow_config = {
-		-- disable = { "" }, --languages that won't include it
-		-- Use parentheses by default, entire tags for HTML and blocks for LaTeX
-		enable = true,
-		query = {
-			"rainbow-parens",
-			html = "rainbow-tags",
-			latex = "rainbow-blocks",
-		},
-		strategy = { rb.strategy["global"] },
-	}
-end
-
 -- checking if it actually loaded
 treesitter.setup({
 	ensure_installed = "all",
@@ -32,5 +16,4 @@ treesitter.setup({
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml" } },
-	rainbow = rainbow_config,
 })
