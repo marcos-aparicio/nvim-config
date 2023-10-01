a1b4cb58b89ac792dd12851b988d247efc47c1f3
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-09-30 22:34:15 -0400

    making gitsigns plugin to use the release branch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,14 @@
 local packer_path = os.getenv("HOME") .. "/.config/nvim/pack"
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = packer_path .. "/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = packer_path .. "/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -18,165 +18,165 @@ packer.init({ package_root = packer_path, opt = false })
 packer.reset()
 
 packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-  use("tpope/vim-abolish")
+	use("wbthomason/packer.nvim")
+	use("tpope/vim-abolish")
 
-  use("tpope/vim-surround")
-  use("tpope/vim-fugitive")
-  use("vim-airline/vim-airline")
-  use("vim-airline/vim-airline-themes")
-  use("ayu-theme/ayu-vim")
-  use({
-    "Fymyte/rasi.vim",
-    ft = "rasi",
-  })
-  --[[ use("mg979/vim-visual-multi") ]]
-  -- rest api testing(like Postman!)
-  use({
-    "rest-nvim/rest.nvim",
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
+	use("tpope/vim-surround")
+	use("tpope/vim-fugitive")
+	use("vim-airline/vim-airline")
+	use("vim-airline/vim-airline-themes")
+	use("ayu-theme/ayu-vim")
+	use({
+		"Fymyte/rasi.vim",
+		ft = "rasi",
+	})
+	--[[ use("mg979/vim-visual-multi") ]]
+	-- rest api testing(like Postman!)
+	use({
+		"rest-nvim/rest.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  -- folding like VSC
-  use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+	-- folding like VSC
+	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
 
-  use("numToStr/Comment.nvim") -- Easily comment stuff
+	use("numToStr/Comment.nvim") -- Easily comment stuff
 
-  use("akinsho/bufferline.nvim")
-  use("moll/vim-bbye")
+	use("akinsho/bufferline.nvim")
+	use("moll/vim-bbye")
 
-  use({ "vimwiki/vimwiki", branch = "dev" })
+	use({ "vimwiki/vimwiki", branch = "dev" })
 
-  use({
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  })
-  use("windwp/nvim-ts-autotag")
-  use("JoosepAlviste/nvim-ts-context-commentstring") -- for comment context
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
+	use("windwp/nvim-ts-autotag")
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- for comment context
 
-  use("easymotion/vim-easymotion")
-  use({
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-    requires = {
-      "tpope/vim-repeat",
-    },
-  })
-  use("sindrets/diffview.nvim")
-  use("lewis6991/gitsigns.nvim")
-  use("norcalli/nvim-colorizer.lua")
+	use("easymotion/vim-easymotion")
+	use({
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+		requires = {
+			"tpope/vim-repeat",
+		},
+	})
+	use("sindrets/diffview.nvim")
+	use({ "lewis6991/gitsigns.nvim", branch = "release" })
+	use("norcalli/nvim-colorizer.lua")
 
-  -- LSP like a pro
-  use({
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  })
+	-- LSP like a pro
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
 
-  -- Completion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-cmdline")
+	-- Completion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
 
-  -- Snippets
-  use("L3MON4D3/LuaSnip") --Snippet Engine
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
-  use("hrsh7th/cmp-nvim-lsp")
+	-- Snippets
+	use("L3MON4D3/LuaSnip") --Snippet Engine
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use("hrsh7th/cmp-nvim-lsp")
 
-  -- Formatting related plugins
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("MunifTanjim/prettier.nvim")
+	-- Formatting related plugins
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
 
-  -- Markdown Live Preview
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  })
+	-- Markdown Live Preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-  use({ "akinsho/toggleterm.nvim", tag = "*" })
+	use({ "akinsho/toggleterm.nvim", tag = "*" })
 
-  -- dadbod (database client inside NEOVIM)
-  use({
-    "kristijanhusak/vim-dadbod-ui",
-    requires = {
-      "tpope/vim-dadbod",
-      "tpope/vim-dotenv",
-    },
-  })
+	-- dadbod (database client inside NEOVIM)
+	use({
+		"kristijanhusak/vim-dadbod-ui",
+		requires = {
+			"tpope/vim-dadbod",
+			"tpope/vim-dotenv",
+		},
+	})
 
-  -- using LeetCode inside Nvim(Awesome!)
-  use("ianding1/leetcode.vim")
+	-- using LeetCode inside Nvim(Awesome!)
+	use("ianding1/leetcode.vim")
 
-  -- nvim-tree and dependencies
-  use({
-    "nvim-tree/nvim-tree.lua",
-    requires = { { "nvim-tree/nvim-web-devicons" } },
-  })
-  -- -- Taskwiki integration in neovim
-  -- use({
-  -- 	"tools-life/taskwiki",
-  -- 	config = function()
-  -- 		vim.g.taskwiki_taskrc_location = os.getenv("HOME") .. "/.config/task/taskrc"
-  -- 		vim.g.taskwiki_data_location = os.getenv("HOME") .. "/.local/share/task"
-  -- 	end,
-  -- })
-  -- nvim-tresitter and extension plugins
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-  })
-  use("HiPhish/rainbow-delimiters.nvim")
+	-- nvim-tree and dependencies
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = { { "nvim-tree/nvim-web-devicons" } },
+	})
+	-- -- Taskwiki integration in neovim
+	-- use({
+	-- 	"tools-life/taskwiki",
+	-- 	config = function()
+	-- 		vim.g.taskwiki_taskrc_location = os.getenv("HOME") .. "/.config/task/taskrc"
+	-- 		vim.g.taskwiki_data_location = os.getenv("HOME") .. "/.local/share/task"
+	-- 	end,
+	-- })
+	-- nvim-tresitter and extension plugins
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
+	use("HiPhish/rainbow-delimiters.nvim")
 
-  -- Telescope and extensions
-  use({
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
-  use("ElPiloto/telescope-vimwiki.nvim")
+	-- Telescope and extensions
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("ElPiloto/telescope-vimwiki.nvim")
 
-  use("natecraddock/workspaces.nvim")
+	use("natecraddock/workspaces.nvim")
 
-  --[[ Marks, File Navigation ]]
-  use("MattesGroeger/vim-bookmarks")
-  use("tom-anders/telescope-vim-bookmarks.nvim")
-  use("ThePrimeagen/harpoon")
+	--[[ Marks, File Navigation ]]
+	use("MattesGroeger/vim-bookmarks")
+	use("tom-anders/telescope-vim-bookmarks.nvim")
+	use("ThePrimeagen/harpoon")
 
-  use({
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-  })
+	use({
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	})
 
-  -- Chat GPT like a pro
-  use({
-    "jackMort/ChatGPT.nvim",
-    config = function() end,
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  })
+	-- Chat GPT like a pro
+	use({
+		"jackMort/ChatGPT.nvim",
+		config = function() end,
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	})
 
-  use("tpope/vim-dispatch")
+	use("tpope/vim-dispatch")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  if packer_bootstrap then
-    packer.sync()
-  end
-  packer.compile()
+	-- Automatically set up your configuration after cloning packer.nvim
+	if packer_bootstrap then
+		packer.sync()
+	end
+	packer.compile()
 end)
