99a92ef7f07d88bf7a6a280c853de12e7351771d
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-10-31 21:53:46 -0400

    updating telescope plugin and adding live grep args telescope extension

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -140,10 +140,11 @@ packer.startup(function(use)
 	-- Telescope and extensions
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.4",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 	use("ElPiloto/telescope-vimwiki.nvim")
+	use("nvim-telescope/telescope-live-grep-args.nvim")
 
 	use("natecraddock/workspaces.nvim")
 
