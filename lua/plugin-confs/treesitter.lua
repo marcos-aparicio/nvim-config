75d63a04a8ad7ac741739a78831ae3285e66863c
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-11-09 10:38:36 -0500

    adding swapping and moving functionality to nvim treesitter text objects

--- lua/plugin-confs/treesitter.lua
+++ lua/plugin-confs/treesitter.lua
@@ -59,5 +59,34 @@ treesitter.setup({
 			-- and should return true of false
 			include_surrounding_whitespace = false,
 		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
 	},
 })
