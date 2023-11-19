f531c797c353e321e0beac314e3621323a10c134
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-11-19 09:12:50 -0500

    deleting vim airline's config info

--- lua/style_settings.lua
+++ lua/style_settings.lua
@@ -1,34 +1,18 @@
 vim.opt.syntax = "on"
 vim.o.termguicolors = true
-
-vim.g.airline_extensions_tabline_left_sep = ""
-vim.g.airline_extensions_tabline_left_sep = ""
-
-vim.g.airline_extensions_tabline_left_alt_sep = ""
-vim.g.airline_extensions_tabline_left_alt_sep = ""
-
-vim.g.airline_left_sep = ""
-vim.g.airline_left_sep = ""
-
-vim.g.airline_right_sep = ""
-vim.g.airline_right_sep = ""
-
-vim.g.airline_powerline_fonts = 1
-vim.g["airline#extensions#tabline#enabled"] = 0
 vim.g.Powerline_symbols = "fancy"
 
-vim.g.airline_theme = "ayu_dark"
 vim.g.ayucolor = "dark"
 
 local ok, _ = pcall(
-  vim.cmd,
-  [[
+	vim.cmd,
+	[[
   colorscheme ayu
   hi Normal guibg=NONE ctermbg=NONE
   ]]
 )
 if not ok then
-  vim.cmd([[
+	vim.cmd([[
     colorscheme default
     hi Normal guibg=NONE ctermbg=NONE
   ]])
