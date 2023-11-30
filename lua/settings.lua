9c7b2c5a6d6e6825e5914edf3ea742ada7285fdd
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-11-30 12:10:13 -0500

    deleting a comment

--- lua/settings.lua
+++ lua/settings.lua
@@ -67,20 +67,3 @@ vim.api.nvim_set_var("terminal_ansi_codes", { ["F15"] = "\27[1;5I", ["F16"] = "\
 
 vim.g.loaded_netwr = 1
 vim.g.loaded_netrwPlugin = 1
-
--- for allowing auto_close
--- nvim-tree is also there in modified buffers so this function filter it out
--- vim.api.nvim_create_autocmd("BufEnter", {
--- 	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
--- 	pattern = "NvimTree_*",
--- 	callback = function()
--- 		local layout = vim.api.nvim_call_function("winlayout", {})
--- 		if
--- 			layout[1] == "leaf"
--- 			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
--- 			and layout[3] == nil
--- 		then
--- 			vim.cmd("confirm quit")
--- 		end
--- 	end,
--- })
