bded8e222eef97b3cf87535c2ee8f12aad25e1dd
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-11-30 12:45:12 -0500

    finally moving from my crazy idea of having all nvim data in .config instead of .local like a normal person

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,6 @@
-local packer_path = os.getenv("HOME") .. "/.config/nvim/pack"
-
 local ensure_packer = function()
 	local fn = vim.fn
-	local install_path = packer_path .. "/packer/start/packer.nvim"
+	local install_path = fn.stdpath("data") .. "site/pack/packer/start/packer.nvim"
 	if fn.empty(fn.glob(install_path)) > 0 then
 		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 		vim.cmd([[packadd packer.nvim]])
@@ -14,7 +12,7 @@ end
 local packer_bootstrap = ensure_packer()
 
 local packer = require("packer")
-packer.init({ package_root = packer_path, opt = false })
+packer.init({ opt = false })
 packer.reset()
 
 packer.startup(function(use)
