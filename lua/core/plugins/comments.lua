e04aca3b40ea7c076714f551b5a57ee501ac6a28
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-12-06 12:58:26 -0500

    adding custom comment for mysql filetype(same as sql filetype)

--- lua/core/plugins/comments.lua
+++ lua/core/plugins/comments.lua
@@ -5,6 +5,8 @@ return {
 		"numToStr/Comment.nvim",
 		lazy = false,
 		config = function()
+			local commentFT = require("Comment.ft")
+
 			require("Comment").setup({
 				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 				opleader = {
@@ -22,6 +24,7 @@ return {
 					extra = true,
 				},
 			})
+			commentFT.mysql = { "--%s" }
 		end,
 	},
 }
