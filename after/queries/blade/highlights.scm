15a20ab250bc4470cea0ce9cf5240613951c9eea
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-12-31 22:31:24 -0500

    adding blade.php treesitter

---
+++ after/queries/blade/highlights.scm
@@ -0,0 +1,11 @@
+;;extends
+
+(directive) @function
+(directive_start) @function
+(directive_end) @function
+((comment) @comment (#set! "priority" 130))
+((parameter) @include (#set! "priority" 110))
+((php_only) @include (#set! "priority" 110))
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
+(keyword) @function
