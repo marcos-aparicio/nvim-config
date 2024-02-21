ee77ed56bd52769a0f946cc6c8d2f4a77fae8ec5
m4rc0s4p4r1c10 <marcos.aparicio-iliquin@mohawkcollege.ca>
2024-02-21 18:06:48 -0500

    moving from null-ls RIP to other formatting and linting plugins

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -1,6 +1,5 @@
 return {
 	"MunifTanjim/prettier.nvim",
-	"nvimtools/none-ls.nvim",
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
@@ -13,14 +12,12 @@ return {
 		dependencies = {
 			"hrsh7th/nvim-cmp",
 			"williamboman/mason-lspconfig.nvim",
-			"nvimtools/none-ls.nvim",
 			"folke/neodev.nvim",
 		},
 		config = function()
 			require("neodev").setup()
 			require("core.lsp.mason")
 			require("core.lsp.handlers").setup()
-			require("core.lsp.null-ls")
 		end,
 	},
 	{
@@ -40,6 +37,15 @@ return {
 					"typescriptreact",
 					"typescript.tsx",
 				},
+			})
+		end,
+	},
+	{
+		"roobert/tailwindcss-colorizer-cmp.nvim",
+		-- optionally, override the default options:
+		config = function()
+			require("tailwindcss-colorizer-cmp").setup({
+				color_square_width = 2,
 			})
 		end,
 	},
