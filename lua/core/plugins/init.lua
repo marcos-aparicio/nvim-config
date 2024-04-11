438740df9a6a9511430d08ab916116eb9ca21589
m4rc0s4p4r1c10 <marcos.aparicio-iliquin@mohawkcollege.ca>
2024-04-11 12:31:02 -0400

    adding noice plugin

--- lua/core/plugins/init.lua
+++ lua/core/plugins/init.lua
@@ -149,8 +149,11 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
+		keys = {
+			{
+				"<leader>1",
+				":Noice dismiss<CR>",
+			},
 		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -158,7 +161,14 @@ return {
 			-- OPTIONAL:
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			{
+				"rcarriga/nvim-notify",
+				config = function()
+					require("notify").setup({
+						background_colour = "#000000",
+					})
+				end,
+			},
 		},
 	},
 }
