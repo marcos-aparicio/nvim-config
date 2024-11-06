cae2e1c4d7062a9f0d5e7d13e3cd6fc5e3850b18
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-11-06 10:58:13 -0500

    adding plugin to delete bad habit of using j and k too much

---
+++ lua/core/plugins/hardtime.lua
@@ -0,0 +1,17 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disabled_filetypes = {
+			"qf",
+			"sagaoutline",
+			"netrw",
+			"NvimTree",
+			"lazy",
+			"mason",
+			"oil",
+			"aerial",
+			"Avante",
+		},
+	},
+}
