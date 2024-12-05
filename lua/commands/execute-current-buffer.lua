8d99c1c6dd85c22fa911f8afca8959c2aca78e7c
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-12-05 17:02:57 -0500

    changing commands module

---
+++ lua/commands/execute-current-buffer.lua
@@ -0,0 +1,52 @@
+local user_cmd = vim.api.nvim_create_user_command
+local HOME = os.getenv("HOME")
+
+local show_error = true
+local function executeCurrentBuffer()
+	local filetype = vim.o.filetype
+	local current_file = vim.fn.expand("%:p")
+	local curr_dir = vim.fn.expand("%:p:h")
+	local redirect = show_error and "" or " 2>/dev/null"
+
+	local filetype_command = {
+		["java"] = "java",
+		["python"] = "python",
+		["sh"] = "sh",
+		["lua"] = "lua",
+		["hurl"] = "hurl --verbose",
+		["plt"] = "plot",
+	}
+
+	local command = filetype_command[filetype] or ""
+	if command ~= "" then
+		vim.cmd(":vs")
+		vim.cmd(":term " .. command .. redirect .. " " .. current_file)
+		return
+	end
+
+	local filetype_function = {
+		["javascript"] = function()
+			if string.match(current_file, ".*test.js$") then
+				vim.cmd(":vs")
+				vim.cmd(":term npm run test %")
+				return
+			end
+			command = "node"
+		end,
+		["php"] = function()
+			vim.cmd(":vs")
+			if string.match(current_file, ".*tests/Browser/.*%.php$") then
+				vim.cmd(":term vendor/bin/sail dusk %")
+				return
+			end
+			vim.cmd(":term vendor/bin/sail test %")
+		end,
+	}
+
+	local exec = filetype_function[filetype] or function()
+		print("Not supported filetype")
+	end
+	exec()
+end
+
+user_cmd("ExecuteCurrentBuffer", executeCurrentBuffer, { desc = "Execute the current buffer" })
---
+++ lua/commands/init.lua
@@ -0,0 +1,59 @@
+require("commands.copy-buffer-path")
+require("commands.execute-current-buffer")
+
+local user_cmd = vim.api.nvim_create_user_command
+local HOME = os.getenv("HOME")
+
+user_cmd("OpenAlacrittyReadonly", function()
+	local current_file = vim.fn.expand("%:p")
+	local current_line, current_col = unpack(vim.api.nvim_win_get_cursor(0))
+
+	local alacritty_command =
+		string.format("alacritty --command nvim %s -R '+call cursor(%d, %d)'", current_file, current_line, current_col)
+	vim.fn.jobstart(alacritty_command, {
+		detach = true,
+	})
+end, {
+	desc = "Open alacritty with the current buffer in read-only mode",
+})
+
+local show_error = true
+user_cmd("ToggleShowingError", function()
+	show_error = not show_error
+	if show_error then
+		print("not showing 2>")
+		return
+	end
+	print("showing 2>")
+end, {
+	desc = "Toggle showing 2> when executing current buffer",
+})
+
+local conda_python = false
+user_cmd("ToggleUseCondaPython", function()
+	conda_python = not conda_python
+	if conda_python then
+		print("Using conda python")
+		return
+	end
+	print("Using standard python")
+end, {
+	desc = "Using conda python or not when executing current buffer",
+})
+
+user_cmd("PrintBufferPath", function()
+	local buffer_path = vim.fn.expand("%:p")
+	print("Buffer's absolute path: " .. buffer_path)
+	-- Use the git command to find the root of the git repository
+	local git_root = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null")
+
+	-- Check if the buffer is inside a Git repository
+	if vim.v.shell_error == 0 and git_root ~= "" then
+		-- Convert buffer path to a relative path from the Git root
+		local relative_path = string.sub(buffer_path, string.len(git_root) + 1)
+		print("Buffer's git root path: " .. relative_path)
+	end
+end, {
+	desc = "Show the current buffer's path",
+})
+vim.keymap.set({ "n" }, "<leader>cc", ":PrintBufferPath<CR>")
