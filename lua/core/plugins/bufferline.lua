a5cf3a71b1bb0d03995feccd250d4f1542a98f58
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-12-05 16:27:36 -0500

    refactoring bufferline mappings

--- lua/core/plugins/bufferline.lua
+++ lua/core/plugins/bufferline.lua
@@ -1,13 +1,19 @@
-vim.keymap.set({ "n" }, "<S-Right>", ":BufferLineMoveNext<CR>")
-vim.keymap.set({ "n" }, "<S-Left>", ":BufferLineMovePrev<CR>")
+local keymaps = {
+	{ "n", "<S-Right>", ":BufferLineMoveNext<CR>" },
+	{ "n", "<S-Left>", ":BufferLineMovePrev<CR>" },
+	{ "n", "<S-l>", ":BufferLineCycleNext<CR>" },
+	{ "n", "<S-h>", ":BufferLineCyclePrev<CR>" },
+	{ "n", "<leader>p", ":BufferLineTogglePin<CR>" },
+	{ "n", "<leader>Q", ":BufferLineCloseOthers<CR>" },
+	{ "n", "<S-t>", ":BufferLinePick<CR>" },
+}
 
-vim.keymap.set({ "n" }, "<S-l>", ":BufferLineCycleNext<CR>")
-vim.keymap.set({ "n" }, "<S-h>", ":BufferLineCyclePrev<CR>")
-
-vim.keymap.set({ "n" }, "<leader>p", ":BufferLineTogglePin<CR>")
-
--- nmap("<leader>Q", ":%bd|e#|bd#<CR>") normal vim way
-vim.keymap.set({ "n" }, "<leader>Q", ":BufferLineCloseOthers<CR>")
+for _, map in ipairs(keymaps) do
+	local opts = { noremap = true, silent = true }
+	-- Merge opts with map[4], if it exists
+	local final_opts = map[4] and vim.tbl_extend("force", opts, map[4]) or opts
+	vim.keymap.set(map[1], map[2], map[3], final_opts)
+end
 
 return {
 	"akinsho/bufferline.nvim",
