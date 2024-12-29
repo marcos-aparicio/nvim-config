38c81796aad1cc3bedfbcc756e0f72d7c8919cfc
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-12-29 02:41:37 -0500

    adding lazy events and loading vscode snippets here

--- lua/core/plugins/snippets/init.lua
+++ lua/core/plugins/snippets/init.lua
@@ -1,6 +1,7 @@
 return {
 	"L3MON4D3/LuaSnip",
-	dependencies = { "hrsh7th/nvim-cmp", "rafamadriz/friendly-snippets" },
+	dependencies = { "hrsh7th/nvim-cmp", "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 	config = function()
 		local languages = {
 			"markdown",
@@ -12,6 +13,7 @@ return {
 			"markdown",
 			"php",
 		}
+		require("luasnip.loaders.from_vscode").lazy_load()
 		for _, language in pairs(languages) do
 			pcall(require, "core.plugins.snippets.settings." .. language)
 		end
