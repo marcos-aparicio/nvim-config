8db160dab45d673fa07eeef8492c9f6525684e2b
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-12-29 02:37:52 -0500

    deleting ufo file and init  file from lsp that i was not using

--- lua/core/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local ok, _ = pcall(require, "lspconfig")
-if not ok then
-	print("lspconfig is not being loaded")
-	return
-end
-
-require("plugin-confs.lsp.mason")
-require("plugin-confs.lsp.handlers").setup()
-require("plugin-confs.lsp.null-ls")
