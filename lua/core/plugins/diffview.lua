430f39ea84e81b49eeaa7201523720272c2b5d2b
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-04-10 11:37:29 -0400

    adding  new diffview plugin

---
+++ lua/core/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+	"sindrets/diffview.nvim",
+	lazy = false,
+	keys = {
+		{ "<leader>gd", ":DiffviewFileHistory %" },
+	},
+}
--- lua/core/plugins/neogit.lua
+++ lua/core/plugins/neogit.lua
@@ -3,8 +3,6 @@ return {
 	cmd = "Neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
-
 		-- Only one of these is needed.
 		"nvim-telescope/telescope.nvim", -- optional
 	},
