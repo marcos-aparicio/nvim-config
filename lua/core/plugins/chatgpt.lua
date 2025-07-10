ca24682014e49be6a17fc20b5283e3850e1ebcfd
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-07-10 17:13:08 -0400

    using gpt-4o-mini as the default model in chatgpt nvim

--- lua/core/plugins/chatgpt.lua
+++ lua/core/plugins/chatgpt.lua
@@ -1,18 +1,21 @@
 return {
-	"jackMort/ChatGPT.nvim",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
-	opts = {
-		api_key_cmd = "pass show personal/api_keys/openai",
-	},
-	keys = {
-		{ "<leader>aie", ":ChatGPTEditWithInstructions<CR>", mode = { "v", "n" } },
-		{ "<leader>aiq", ":ChatGPT<CR>" },
-		{ "<leader>air", ":ChatGPTRun<space>", mode = { "v", "n" } },
-		{ "<leader>aid", ":ChatGPTRun docstring<CR>", mode = "v" },
-		{ "<leader>ait", ":ChatGPTRun add_tests<CR>", mode = "v" },
-	},
+  "jackMort/ChatGPT.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    api_key = "pass show personal/api_keys/openai",
+    openai_params = {
+      model = "gpt-4o-mini"
+    },
+  },
+  keys = {
+    { "<leader>aie", ":ChatGPTEditWithInstructions<CR>", mode = { "v", "n" } },
+    { "<leader>aiq", ":ChatGPT<CR>" },
+    { "<leader>air", ":ChatGPTRun<space>",               mode = { "v", "n" } },
+    { "<leader>aid", ":ChatGPTRun docstring<CR>",        mode = "v" },
+    { "<leader>ait", ":ChatGPTRun add_tests<CR>",        mode = "v" },
+  },
 }
