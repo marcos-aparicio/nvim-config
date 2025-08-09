1e585b57096cca749c4a5fbeef5314d697d4660a
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-08-09 10:07:23 -0400

    cleaning code in css snippet

--- lua/core/plugins/snippets/settings/css.lua
+++ lua/core/plugins/snippets/settings/css.lua
@@ -4,54 +4,25 @@ if not ls then
 	return
 end
 
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
+local s, t, i, f = ls.snippet, ls.text_node, ls.insert_node, ls.function_node
 
-local MIN_VW_CLAMP = 320
-local MAX_VW_CLAMP = 1240
+local MIN_VW_CLAMP, MAX_VW_CLAMP = 320, 1240
 
-local clamp_logic = function(args, parent_args, user_args)
-	local min = args[1][1]
-	local max = args[2][1]
-
-	local ok, min_px = pcall(tonumber, min)
-	if not ok or min_px == nil then
-		return ""
-	end
-	local ok2, max_px = pcall(tonumber, max)
-	if not ok2 or max_px == nil then
+local function clamp_logic(args, parent_args)
+	local min = tonumber(args[1][1])
+	local max = tonumber(args[2][1])
+	if not min or not max then
 		return ""
 	end
 
-	-- Clamp logic
-	local min_vw = MIN_VW_CLAMP
-	local max_vw = MAX_VW_CLAMP
+	local min_vw = tonumber(parent_args.captures[2]) or MIN_VW_CLAMP
+	local max_vw = tonumber(parent_args.captures[3]) or MAX_VW_CLAMP
 
-	local ok3, custom_min_vw = pcall(tonumber, parent_args.captures[2])
-	if ok3 and custom_min_vw ~= nil then
-		min_vw = custom_min_vw
-	end
-
-	local ok4, custom_max_vw = pcall(tonumber, parent_args.captures[3])
-	if ok4 and custom_max_vw ~= nil then
-		max_vw = custom_max_vw
-	end
-
-	-- Calculate slope and intercept
-	local slope = (max_px - min_px) / (max_vw - min_vw)
+	local slope = (max - min) / (max_vw - min_vw)
 	local slope_vw = slope * 100
-	local intercept = min_px - slope * min_vw
-
-	-- Format intercept with px and parentheses if negative
+	local intercept = min - slope * min_vw
 	local intercept_str = intercept < 0 and string.format("(%.3fpx)", intercept) or string.format("%.3fpx", intercept)
-
-	-- Format final clamp string
-	local clamp = string.format("%s + %.3fvw", intercept_str, slope_vw)
-
-	return clamp
-	-- return min .. " " .. max .. " px"
+	return string.format("%s + %.3fvw", intercept_str, slope_vw)
 end
 local css_snippets = {
 	s("fuen", {
@@ -66,12 +37,12 @@ local css_snippets = {
 		t("px);"),
 	}),
 	s({ trig = "clamp:([%w_-]+):?([%d]*):?([%d]*)", regTrig = true }, {
-		f(function(args, snip)
+		f(function(_, snip)
 			return snip.captures[1] .. ": "
 		end, {}),
 		i(1),
 		t({ "; /* fallback */", "" }),
-		f(function(args, snip)
+		f(function(_, snip)
 			return snip.captures[1] .. ": clamp("
 		end, {}),
 		i(2, "min size(in px)"),
@@ -80,20 +51,9 @@ local css_snippets = {
 		t(", "),
 		i(3, "max size(in px)"),
 		t("px); "),
-		f(function(args, snip)
-			-- Clamp logic
-			local min_vw = MIN_VW_CLAMP
-			local max_vw = MAX_VW_CLAMP
-
-			local ok3, custom_min_vw = pcall(tonumber, snip.captures[2])
-			if ok3 and custom_min_vw ~= nil then
-				min_vw = custom_min_vw
-			end
-
-			local ok4, custom_max_vw = pcall(tonumber, snip.captures[3])
-			if ok4 and custom_max_vw ~= nil then
-				max_vw = custom_max_vw
-			end
+		f(function(_, snip)
+			local min_vw = tonumber(snip.captures[2]) or MIN_VW_CLAMP
+			local max_vw = tonumber(snip.captures[3]) or MAX_VW_CLAMP
 			return string.format(
 				"/* Property will dynamically change between %dpx and %dpx of viewport width */",
 				min_vw,
@@ -115,12 +75,12 @@ local scss_snippets = {
 		t("px)');"),
 	}),
 	s({ trig = "clamp:([%w_-]+):?([%d]*):?([%d]*)", regTrig = true }, {
-		f(function(args, snip)
+		f(function(_, snip)
 			return snip.captures[1] .. ": "
 		end, {}),
 		i(1),
 		t({ "; /* fallback */", "" }),
-		f(function(args, snip)
+		f(function(_, snip)
 			return snip.captures[1] .. ": unquote('clamp("
 		end, {}),
 		i(2, "min size(in px)"),
@@ -129,20 +89,9 @@ local scss_snippets = {
 		t(", "),
 		i(3, "max size(in px)"),
 		t("px)'); "),
-		f(function(args, snip)
-			-- Clamp logic
-			local min_vw = MIN_VW_CLAMP
-			local max_vw = MAX_VW_CLAMP
-
-			local ok3, custom_min_vw = pcall(tonumber, snip.captures[2])
-			if ok3 and custom_min_vw ~= nil then
-				min_vw = custom_min_vw
-			end
-
-			local ok4, custom_max_vw = pcall(tonumber, snip.captures[3])
-			if ok4 and custom_max_vw ~= nil then
-				max_vw = custom_max_vw
-			end
+		f(function(_, snip)
+			local min_vw = tonumber(snip.captures[2]) or MIN_VW_CLAMP
+			local max_vw = tonumber(snip.captures[3]) or MAX_VW_CLAMP
 			return string.format(
 				"/* Property will dynamically change between %dpx and %dpx of viewport width */",
 				min_vw,
