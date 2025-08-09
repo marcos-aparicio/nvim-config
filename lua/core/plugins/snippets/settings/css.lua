bb296b8a0eb37c2eedccdf761e6c7cce4e23a5dc
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-08-09 09:47:46 -0400

    adding better clamp snippet for css and scss

--- lua/core/plugins/snippets/settings/css.lua
+++ lua/core/plugins/snippets/settings/css.lua
@@ -9,6 +9,9 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 
+local MIN_VW_CLAMP = 320
+local MAX_VW_CLAMP = 1240
+
 local clamp_logic = function(args, parent_args, user_args)
 	local min = args[1][1]
 	local max = args[2][1]
@@ -23,8 +26,19 @@ local clamp_logic = function(args, parent_args, user_args)
 	end
 
 	-- Clamp logic
-	local min_vw = 320
-	local max_vw = 1240
+	local min_vw = MIN_VW_CLAMP
+	local max_vw = MAX_VW_CLAMP
+
+	local ok3, custom_min_vw = pcall(tonumber, parent_args.captures[2])
+	if ok3 and custom_min_vw ~= nil then
+		min_vw = custom_min_vw
+	end
+
+	local ok4, custom_max_vw = pcall(tonumber, parent_args.captures[3])
+	if ok4 and custom_max_vw ~= nil then
+		max_vw = custom_max_vw
+	end
+
 	-- Calculate slope and intercept
 	local slope = (max_px - min_px) / (max_vw - min_vw)
 	local slope_vw = slope * 100
@@ -51,9 +65,44 @@ local css_snippets = {
 		i(3, "max size(in px)"),
 		t("px);"),
 	}),
+	s({ trig = "clamp:([%w_-]+):?([%d]*):?([%d]*)", regTrig = true }, {
+		f(function(args, snip)
+			return snip.captures[1] .. ": "
+		end, {}),
+		i(1),
+		t({ "; /* fallback */", "" }),
+		f(function(args, snip)
+			return snip.captures[1] .. ": clamp("
+		end, {}),
+		i(2, "min size(in px)"),
+		t("px, "),
+		f(clamp_logic, { 2, 3 }),
+		t(", "),
+		i(3, "max size(in px)"),
+		t("px); "),
+		f(function(args, snip)
+			-- Clamp logic
+			local min_vw = MIN_VW_CLAMP
+			local max_vw = MAX_VW_CLAMP
+
+			local ok3, custom_min_vw = pcall(tonumber, snip.captures[2])
+			if ok3 and custom_min_vw ~= nil then
+				min_vw = custom_min_vw
+			end
+
+			local ok4, custom_max_vw = pcall(tonumber, snip.captures[3])
+			if ok4 and custom_max_vw ~= nil then
+				max_vw = custom_max_vw
+			end
+			return string.format(
+				"/* Property will dynamically change between %dpx and %dpx of viewport width */",
+				min_vw,
+				max_vw
+			)
+		end),
+	}),
 }
 local scss_snippets = {
-
 	s("fuen", {
 		t("font-size: "),
 		i(1, "fallback font size(in px)"),
@@ -64,6 +113,42 @@ local scss_snippets = {
 		t(", "),
 		i(3, "max size(in px)"),
 		t("px)');"),
+	}),
+	s({ trig = "clamp:([%w_-]+):?([%d]*):?([%d]*)", regTrig = true }, {
+		f(function(args, snip)
+			return snip.captures[1] .. ": "
+		end, {}),
+		i(1),
+		t({ "; /* fallback */", "" }),
+		f(function(args, snip)
+			return snip.captures[1] .. ": unquote('clamp("
+		end, {}),
+		i(2, "min size(in px)"),
+		t("px, "),
+		f(clamp_logic, { 2, 3 }),
+		t(", "),
+		i(3, "max size(in px)"),
+		t("px)'); "),
+		f(function(args, snip)
+			-- Clamp logic
+			local min_vw = MIN_VW_CLAMP
+			local max_vw = MAX_VW_CLAMP
+
+			local ok3, custom_min_vw = pcall(tonumber, snip.captures[2])
+			if ok3 and custom_min_vw ~= nil then
+				min_vw = custom_min_vw
+			end
+
+			local ok4, custom_max_vw = pcall(tonumber, snip.captures[3])
+			if ok4 and custom_max_vw ~= nil then
+				max_vw = custom_max_vw
+			end
+			return string.format(
+				"/* Property will dynamically change between %dpx and %dpx of viewport width */",
+				min_vw,
+				max_vw
+			)
+		end),
 	}),
 }
 
