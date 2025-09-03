a2b5766b8b93a71020c7cc6e3a5550d346c870fa
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-09-03 12:46:49 -0400

    providing both ai assistant options in one and changing mappings for reset in copilotchat.nvim

--- lua/core/plugins/ai_assistant.lua
+++ lua/core/plugins/ai_assistant.lua
@@ -68,53 +68,7 @@ local copilotchat_config = {
 }
 
 if USE_CODECOMPANION then
-  return {
-    {
-      "olimorris/codecompanion.nvim",
-      opts = {
-        extensions = {
-          vectorcode = {
-            opts = {
-              tool_group = {
-                enabled = true,
-                extras = {},
-                collapse = false,
-              },
-              tool_opts = {
-                ["*"] = {},
-                ls = {},
-                vectorise = {},
-                query = {
-                  max_num = { chunk = -1, document = -1 },
-                  default_num = { chunk = 50, document = 10 },
-                  include_stderr = false,
-                  use_lsp = false,
-                  no_duplicate = true,
-                  chunk_mode = false,
-                  summarise = {
-                    enabled = false,
-                    adapter = nil,
-                    query_augmented = true,
-                  },
-                },
-                files_ls = {},
-                files_rm = {},
-              },
-            },
-          },
-        },
-      },
-      keys = {
-        { "<leader>ac", ":codecompanionchat toggle<cr>" },
-        { "<leader>aa", ":codecompanionactions <cr>" },
-        { "<leader>ap", ":codecompanion " },
-      },
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-      },
-    },
-  }
+  return { codecompanion_config }
 else
 	return {
 		{
