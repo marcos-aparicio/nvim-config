3193ef89d3871c2ea1b6771a54102d5dbe6a6982
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-09-03 12:46:49 -0400

    providing both ai assistant options in one and changing mappings for reset in copilotchat.nvim

--- lua/core/plugins/ai_assistant.lua
+++ lua/core/plugins/ai_assistant.lua
@@ -1,69 +1,74 @@
 local USE_CODECOMPANION = false -- Set to false to use CopilotChat
 
+local codecompanion_config = {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    -- extensions = {
+    --   vectorcode = {
+    --     opts = {
+    --       tool_group = {
+    --         enabled = true,
+    --         extras = {},
+    --         collapse = false,
+    --       },
+    --       tool_opts = {
+    --         ["*"] = {},
+    --         ls = {},
+    --         vectorise = {},
+    --         query = {
+    --           max_num = { chunk = -1, document = -1 },
+    --           default_num = { chunk = 50, document = 10 },
+    --           include_stderr = false,
+    --           use_lsp = false,
+    --           no_duplicate = true,
+    --           chunk_mode = false,
+    --           summarise = {
+    --             enabled = false,
+    --             adapter = nil,
+    --             query_augmented = true,
+    --           },
+    --         },
+    --         files_ls = {},
+    --         files_rm = {},
+    --       },
+    --     },
+    --   },
+    -- },
+  },
+  keys = {
+    { "<leader>at", ":CodeCompanionChat toggle<CR>" },
+    { "<leader>aa", ":CodeCompanionActions <CR>" },
+    { "<leader>ap", ":CodeCompanion " },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+
+local copilotchat_config = {
+  "copilotc-nvim/copilotchat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  keys = { { "<leader>ac", ":CopilotChatToggle<cr>" } },
+  build = "make tiktoken",
+  opts = {
+    mappings = {
+      show_help = {
+        normal = "?"
+      },
+      reset = {
+        insert = "",
+        normal = "grs"
+      }
+    }
+  },
+}
+
 if USE_CODECOMPANION then
-  return {
-    {
-      "olimorris/codecompanion.nvim",
-      opts = {
-        extensions = {
-          vectorcode = {
-            opts = {
-              tool_group = {
-                enabled = true,
-                extras = {},
-                collapse = false,
-              },
-              tool_opts = {
-                ["*"] = {},
-                ls = {},
-                vectorise = {},
-                query = {
-                  max_num = { chunk = -1, document = -1 },
-                  default_num = { chunk = 50, document = 10 },
-                  include_stderr = false,
-                  use_lsp = false,
-                  no_duplicate = true,
-                  chunk_mode = false,
-                  summarise = {
-                    enabled = false,
-                    adapter = nil,
-                    query_augmented = true,
-                  },
-                },
-                files_ls = {},
-                files_rm = {},
-              },
-            },
-          },
-        },
-      },
-      keys = {
-        { "<leader>ac", ":codecompanionchat toggle<cr>" },
-        { "<leader>aa", ":codecompanionactions <cr>" },
-        { "<leader>ap", ":codecompanion " },
-      },
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-      },
-    },
-  }
+  return { codecompanion_config }
 else
-  return {
-    {
-      "copilotc-nvim/copilotchat.nvim",
-      dependencies = {
-        { "nvim-lua/plenary.nvim", branch = "master" },
-      },
-      keys = { { "<leader>ac", ":CopilotChatToggle<cr>" } },
-      build = "make tiktoken",
-      opts = {
-        mappings = {
-          show_help = {
-            normal = "?"
-          }
-        }
-      },
-    },
-  }
+  return { copilotchat_config, codecompanion_config }
 end
