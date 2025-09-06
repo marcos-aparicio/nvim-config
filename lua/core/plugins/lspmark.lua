aa8436702e21be8a0c5a435a6d8b83b31a7e8551
marcos-aparicio <marcos.aparicio@pm.me>
2025-09-06 19:38:51 -0400

    changing keybindings of lspmark plugin

--- lua/core/plugins/lspmark.lua
+++ lua/core/plugins/lspmark.lua
@@ -1,12 +1,12 @@
 return {
-  "tristone13th/lspmark.nvim",
-  main = "lspmark",
-  keys = {
-    { "mm", ":lua require('lspmark.bookmarks').toggle_bookmark({with_comment=true})<CR>" },
-    { "ma", ":Telescope lspmark<CR>" },
-    { "dd", ":lua require('lspmark.bookmarks').delete_line()<CR>" },
-    { "p",  ":lua require('lspmark.bookmarks').paste_text()<CR>" },
-    { "mi", ":lua require('lspmark.bookmarks').modify_comment()<CR>" }
-  },
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	"tristone13th/lspmark.nvim",
+	main = "lspmark",
+	keys = {
+		{ "mm", ":lua require('lspmark.bookmarks').toggle_bookmark({with_comment=true})<CR>" },
+		{ "ma", ":Telescope lspmark<CR>" },
+		{ "md", ":lua require('lspmark.bookmarks').delete_line()<CR>" },
+		{ "mp", ":lua require('lspmark.bookmarks').paste_text()<CR>" },
+		{ "mi", ":lua require('lspmark.bookmarks').modify_comment()<CR>" },
+	},
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 }
