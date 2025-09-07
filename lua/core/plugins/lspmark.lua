1b1bf6a82d84e8b4b2ba1e6c8b65d90e50d0b295
marcos-aparicio <marcos.aparicio@pm.me>
2025-09-07 16:18:20 -0400

    correcting lspmark auto save to work as expected(i was misinterpreting it)

--- lua/core/plugins/lspmark.lua
+++ lua/core/plugins/lspmark.lua
@@ -1,6 +1,5 @@
 return {
 	"tristone13th/lspmark.nvim",
-	main = "lspmark",
 	keys = {
 		{ "mm", ":lua require('lspmark.bookmarks').toggle_bookmark({with_comment=true})<CR>" },
 		{ "ma", ":Telescope lspmark<CR>" },
@@ -8,5 +7,17 @@ return {
 		{ "mp", ":lua require('lspmark.bookmarks').paste_text()<CR>" },
 		{ "mi", ":lua require('lspmark.bookmarks').modify_comment()<CR>" },
 	},
-	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	-- event = { "DirChanged" },
+	lazy = false,
+	config = function()
+		require("lspmark").setup()
+		require("lspmark.bookmarks").load_bookmarks() -- so that it also loads on startup
+		vim.api.nvim_create_autocmd({ "DirChanged" }, {
+			callback = function()
+				vim.notify("i happen crack")
+				require("lspmark.bookmarks").load_bookmarks()
+			end,
+			pattern = { "*" },
+		})
+	end,
 }
