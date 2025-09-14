e995cb09b7a5d10c5a6cce01ebb4366cd64986c2
marcos-aparicio <marcos.aparicio@pm.me>
2025-09-14 05:08:02 -0400

    adding new highlight colors for cmp and markdown to be more aesthetic

---
+++ lua/core/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {
+			-- Customize bullet icons
+			bullet = {
+        icons = { '• ', '‣ ', '∙ ', '◦ ' }, -- Small and clean bullet icons
+			},
+    },
+	},
+}
