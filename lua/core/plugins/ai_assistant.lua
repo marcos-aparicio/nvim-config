da927b3915b00969222f9400bd79adf2504137fb
marcos-aparicio <marcos.aparicio@pm.me>
2025-09-27 12:42:20 -0400

    adding keymappings for easily saving and loading copilotchat.nvim conversations

--- lua/core/plugins/ai_assistant.lua
+++ lua/core/plugins/ai_assistant.lua
@@ -97,49 +97,6 @@ else
 						normal = "grs",
 					},
 				},
-				prompts = {
-					Quickfix = {
-						system_prompt = "The user will provide you with some context and instructions and you should respond in an output format that can be interpreted by a quickfix file in the following format per line: <filename>:<column>:<line>:<text> ",
-					},
-				},
-        functions ={
-          dir_tree = {
-            description = "Returns a tree view of the given directory path",
-            uri = "tree://{path}",
-            schema = {
-              type = 'object',
-              required = { 'path' },
-              properties = {
-                path = {
-                  type = 'string',
-                  description = "Directory path to display as a tree",
-                  default = vim.fn.getcwd(), -- Add this line
-                },
-              },
-            },
-            resolve = function(input)
-              local max_level = 4 -- set your desired depth
-
-              local command = string.format('tree -L %d --prune -I "node_modules" "%s"', max_level, input.path)
-              local handle = io.popen(command)
-              local result = ""
-              if handle then
-                result = handle:read("*a")
-                handle:close()
-              else
-                result = "Error: Unable to open directory or run 'tree' command."
-              end
-              return {
-                {
-                  uri = 'tree://' .. input.path,
-                  mimetype = 'text/plain',
-                  data = result,
-                }
-              }
-            end
-          }
-        }
-
 			},
 		},
 	}
