83b7e72c34b2b8148c4a95f3e230d056b59ac793
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-10-24 10:08:32 -0400

    Adding case insensitive flash search

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -1,3 +1,16 @@
+local function with_case_settings(callback)
+  local gi, gs = vim.go.ignorecase, vim.go.smartcase
+  vim.go.ignorecase = true
+  vim.go.smartcase = false
+  local ok, result = pcall(callback)
+  vim.go.ignorecase = gi
+  vim.go.smartcase = gs
+  if not ok then
+    error(result)
+  end
+  return result
+end
+
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
@@ -14,10 +27,53 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        with_case_settings(function()
+          require("flash").jump()
+        end)
+      end,
+      desc = "Flash"
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        with_case_settings(function()
+          require("flash").treesitter()
+        end)
+      end,
+      desc = "Flash Treesitter"
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        with_case_settings(function()
+          require("flash").remote()
+        end)
+      end,
+      desc = "Remote Flash"
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        with_case_settings(function()
+          require("flash").treesitter_search()
+        end)
+      end,
+      desc = "Treesitter Search"
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search"
+    },
   },
 }
