dacc87c2ad7a59d0b3478a39f4a9650660e73386
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-11-03 16:43:35 -0500

    fix(formatting): correct FormatDisable! buffer logic
    Previously, FormatDisable! set disable_autoformat to true, which was
    incorrect. Now, it properly sets disable_autoformat to false for the
    current buffer, ensuring formatting is enabled as intended.

--- lua/core/plugins/formatting.lua
+++ lua/core/plugins/formatting.lua
@@ -3,7 +3,7 @@ vim.b.disable_autoformat = true
 vim.api.nvim_create_user_command("FormatDisable", function(args)
 	if args.bang then
 		-- FormatDisable! will disable formatting just for this buffer
-		vim.b.disable_autoformat = true
+		vim.b.disable_autoformat = false
 	else
 		vim.g.disable_autoformat = true
 	end
