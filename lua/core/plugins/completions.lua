cdd67a7f92bebcb8f8bea51030de1b6486e31998
marcos-aparicio <marcos.aparicio@pm.me>
2025-11-12 09:31:59 -0500

    refactor(completions): improve blink.cmp config structure
    Reformats the blink.cmp plugin configuration for better readability and
    maintainability. Adds SQL and MySQL filetype-specific sources, clarifies
    provider definitions, and adjusts indentation for consistency.

--- lua/core/plugins/completions.lua
+++ lua/core/plugins/completions.lua
@@ -1,96 +1,96 @@
 return {
-	"saghen/blink.cmp",
+  "saghen/blink.cmp",
   -- version = "v0.13.1",
   dependencies = {
     "moyiz/blink-emoji.nvim",
     "Kaiser-Yang/blink-cmp-dictionary",
     "L3MON4D3/LuaSnip"
   },
-	-- use a release tag to download pre-built binaries
-	version = "1.*",
-	build = "cargo build --release",
-	event = "InsertEnter",
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-			keymap = {
-				preset = "default",
-				["<C-k>"] = { "select_prev", "fallback" },
-				["<C-j>"] = { "select_next", "fallback" },
-				["<CR>"] = {
-					function(cmp)
-						if cmp.snippet_active() then
-							return cmp.accept()
-						else
-							return cmp.select_and_accept()
-						end
-					end,
-					"snippet_forward",
-					"fallback",
-				},
-				-- ["<Tab>"] = {
-				-- 	function(cmp)
-				-- 		if cmp.snippet_active() then
-				-- 			return cmp.accept()
-				-- 		else
-				-- 			return cmp.select_and_accept()
-				-- 		end
-				-- 	end,
-				-- 	"snippet_forward",
-				-- 	"fallback",
-				-- },
-				["<S-Tab>"] = { "snippet_backward", "fallback" },
-        ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<S-j>"] = { "scroll_documentation_down", "fallback" },
-			},
-			appearance = {
-				nerd_font_variant = "mono",
-				use_nvim_cmp_as_default = true,
-			},
-			completion = {
-        documentation = { auto_show = true },
-				menu = {
-					winblend = 0,
-					winhighlight = 'Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None',
-					scrollbar = true,
-					direction_priority = { 's', 'n' },
-					auto_show = true,
-					draw = {
-						padding = 1,
-						gap = 1,
-					},
-				},
+  -- use a release tag to download pre-built binaries
+  version = "1.*",
+  build = "cargo build --release",
+  event = "InsertEnter",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<CR>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
       },
-			snippets = {
-				preset = "luasnip",
-			},
-			sources = {
-				default = { "snippets", "path", "lsp", "buffer", "emoji"  },
-        per_filetype = {
-          sql = { "dadbod" }
+      -- ["<Tab>"] = {
+      -- 	function(cmp)
+      -- 		if cmp.snippet_active() then
+      -- 			return cmp.accept()
+      -- 		else
+      -- 			return cmp.select_and_accept()
+      -- 		end
+      -- 	end,
+      -- 	"snippet_forward",
+      -- 	"fallback",
+      -- },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+      use_nvim_cmp_as_default = true,
+    },
+    completion = {
+      documentation = { auto_show = true },
+      menu = {
+        winblend = 0,
+        winhighlight = 'Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None',
+        scrollbar = true,
+        direction_priority = { 's', 'n' },
+        auto_show = true,
+        draw = {
+          padding = 1,
+          gap = 1,
         },
-        providers = {
-          dadbod = { module = "vim_dadbod_completion.blink" },
-          snippets = {
-            name = "snippets",
-            enabled = true,
-            max_items = 15,
-            min_keyword_length = 2,
-            module = "blink.cmp.sources.snippets",
-            score_offset = 85, -- the higher the number, the higher the priority
-				},
+      },
+    },
+    snippets = {
+      preset = "luasnip",
+    },
+    sources = {
+      default = { "snippets", "path", "lsp", "buffer", "emoji" },
+      per_filetype = {
+        sql = { "snippets", "dadbod" },
+        mysql = { "snippets", "dadbod" }
+      },
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        snippets = {
+          name = "snippets",
+          enabled = true,
+          max_items = 15,
+          min_keyword_length = 2,
+          module = "blink.cmp.sources.snippets",
+          score_offset = 85, -- the higher the number, the higher the priority
+        },
         emoji = {
           module = "blink-emoji",
           name = "Emoji",
-          score_offset = 93, -- the higher the number, the higher the priority
+          score_offset = 93,        -- the higher the number, the higher the priority
           min_keyword_length = 2,
           opts = { insert = true }, -- Insert emoji (default) or complete its name
         },
 
-        }
-			},
+      }
+    },
   },
-	opts_extend = { "sources.default" },
+  opts_extend = { "sources.default" },
 }
-
