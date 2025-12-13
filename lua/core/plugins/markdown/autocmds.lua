f8de5d362d61c51bed36529e73bfaa335b271af5
marcos-aparicio <marcos.aparicio@pm.me>
2025-12-12 20:38:40 -0500

    refactor(markdown): simplify autocmds for buffer setup
    Refactored markdown autocmds to use a shared setup function for buffer
    keymaps and commands. Now uses 'pattern' for FileType markdown and
    BufEnter for files in the notes workspace directory, improving clarity
    and maintainability.

--- lua/core/plugins/markdown/autocmds.lua
+++ lua/core/plugins/markdown/autocmds.lua
@@ -1,22 +1,24 @@
 local globals = require "globals"
 local M = {}
 
+local function setup_markdown_features()
+  local keymaps = require("core.plugins.markdown.keymaps")
+  local commands = require("core.plugins.markdown.cmds")
+  keymaps.setup_buffer_keymaps()
+  commands.setup_buffer_commands()
+end
+
 function M.setup()
+  -- For markdown files
   vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-      local ft = vim.bo[args.buf].filetype
-      local path = vim.api.nvim_buf_get_name(args.buf)
-      if not path or  not (
-            ft == "markdown" or vim.startswith(path, globals.notes_workspaces_dir)
-            or (path == "" and vim.startswith(vim.fn.getcwd(), globals.notes_workspaces_dir))
-          ) then
-        return
-      end
-      local keymaps = require("core.plugins.markdown.keymaps")
-      local commands = require("core.plugins.markdown.cmds")
-      keymaps.setup_buffer_keymaps()
-      commands.setup_buffer_commands()
-    end,
+    pattern = "markdown",
+    callback = setup_markdown_features,
+  })
+
+  -- For files in notes workspace directory
+  vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = globals.notes_workspaces_dir .. "/*",
+    callback = setup_markdown_features,
   })
 end
 
