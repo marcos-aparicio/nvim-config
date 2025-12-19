5ae561582cc0970b97093f09039d84aede72ddd2
marcos-aparicio <marcos.aparicio@pm.me>
2025-12-19 09:33:21 -0500

    refactor(alpha): remove Settings button from dashboard
    The Settings button was removed from the Alpha dashboard to simplify the
    interface and reduce clutter. I was getting annoyed with that binding
    ngl

--- lua/core/plugins/alpha.lua
+++ lua/core/plugins/alpha.lua
@@ -28,7 +28,6 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("r", "  > Recent", ":Telescope zoxide list<CR>"),
-			dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 			dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 		}
 
