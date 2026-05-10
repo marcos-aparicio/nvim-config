f1b670914fd4f6b85cf47c2fceba41eb668f87fd
marcos-aparicio <marcos.aparicio@pm.me>
2026-05-10 11:38:04 -0400

    feat: add treesitter parser for crystal

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -147,6 +147,22 @@ return {
           vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
       })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = 'TSUpdate',
+        callback = function()
+          require('nvim-treesitter.parsers').crystal = {
+            install_info = {
+              url = 'https://github.com/crystal-lang-tools/tree-sitter-crystal',
+              -- path = '<ts-crystal-install-path>', -- if you want to use a local version instead
+              generate = false,
+              generate_from_json = false,
+              queries = 'queries/nvim'
+            },
+          }
+        end,
+      })
+
+      vim.treesitter.language.register("crystal", { "cr" })
       local ensureInstalled = {
         'lua', 'python', 'typescript', 'javascript', 'javascriptreact', 'http',
         'html', 'scss', 'css', 'yaml', 'toml'
