67f36adf538384a3c1e5e911afaab4a605e621b2
marcos-aparicio <marcos.aparicio@pm.me>
2026-06-12 19:31:31 -0400

    feat(lualine): use pywal for theme

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -2,50 +2,13 @@ local function get_spell_status()
   return vim.bo.spelllang
 end
 
-local colors = {
-  color1 = nil,
-  color2 = "#242b38",
-  -- color2 = nil,
-  color3 = "#d4bfff",
-  color4 = "#d9d7ce",
-  color5 = "#272d38",
-  color13 = "#bbe67e",
-  color10 = "#59c2ff",
-  color8 = "#f07178",
-  color9 = "#607080",
-}
-
-local custom_ayu_mirage = {
-  visual = {
-    a = { fg = colors.color2, bg = colors.color3, gui = "bold" },
-    b = { fg = colors.color4, bg = colors.color5 },
-  },
-  replace = {
-    a = { fg = colors.color2, bg = colors.color8, gui = "bold" },
-    b = { fg = colors.color4, bg = colors.color5 },
-  },
-  inactive = {
-    c = { fg = colors.color4, bg = colors.color1 },
-    a = { fg = colors.color4, bg = colors.color5, gui = "bold" },
-    b = { fg = colors.color4, bg = colors.color5 },
-  },
-  normal = {
-    c = { fg = colors.color9, bg = colors.color1 },
-    a = { fg = colors.color2, bg = colors.color13, gui = "bold" },
-    b = { fg = colors.color4, bg = colors.color5 },
-  },
-  insert = {
-    a = { fg = colors.color2, bg = colors.color10, gui = "bold" },
-    b = { fg = colors.color4, bg = colors.color5 },
-  },
-}
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
-      theme = custom_ayu_mirage,
+      theme = 'pywal',
       component_separators = "|",
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
